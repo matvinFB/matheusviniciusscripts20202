5e963fbc66e2fb3b9e09294bb5d24367b4e9a493
João Marcelo Uchôa de Alencar <jmarcelo.alencar@gmail.com>
2020-12-14 09:17:08 -0300

    Correção.

--- atividades/atividade02/maiorQue10mb.sh
+++ atividades/atividade02/maiorQue10mb.sh
@@ -1,3 +1,4 @@
+# Correção: OK. 1,0 ponto. Apenas se acostume a colocar #!/bin/bash no início de cada script.
 mkdir maiorque10
 find ./ -size +10M -exec mv {} ./maiorque10/ \;
 tar -czvf maiorque10.tar.gz maiorque10/
