38dcb3cf2fa891350d992780a2dd7bc52c79ac8a
Matvin <matheusviniciusf@gmail.com>
2020-12-23 23:01:10 -0300

    atividade 04

---
+++ atividades/atividade04/codigo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sed -i 's/#!\/bin\/python/\/usr\/bin\/python3/' atividade04.py
+sed -i -r 's/nota1|nota2|notaFinal/\U&/g' atividade04.py
+sed -i '3a \import  time' atividade04.py
+sed -i '$a \   print time.ctime\(\)' atividade04.py
+
