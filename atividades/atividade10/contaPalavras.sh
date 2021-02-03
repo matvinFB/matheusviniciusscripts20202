24c1f20633662948c2ac4150f9cff80c4d1415aa
MatvinFB <matheusviniciusf@gmail.com>
2021-02-03 19:29:33 -0300

    atividade 10 questao 2

---
+++ atividades/atividade10/contaPalavras.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+printf "Informe o arquivo: "
+read arq
+
+tr ' ' '\n' <$arq >tempt.txt
+awk 'NF' tempt.txt >temptt.txt
+tr -d '“\056\077\072\054\042\133\135\134' <temptt.txt >temp.txt
+rm tempt.txt
+rm temptt.txt
+
+#cat temp.txt
+
+
+for i in $(sort -u temp.txt | cut -d " " -f2)
+do
+	echo "$i : $(grep -w $i temp.txt | wc -l)"
+done
+
+rm temp.txt
+
